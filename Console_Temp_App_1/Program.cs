b664229b135173b78a73e4e4f14daa946810714d
Tejpartap Singh <singh.tejpartap@gmail.com>
2019-09-05 13:35:19 +1000

    Add project files.

---
+++ Console_Temp_App_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Console_Temp_App_1
+{
+    abstract class A
+    {
+        protected A() { Console.WriteLine("Abstract class constructor"); }
+    }
+    //Derived class
+    class B : A
+    {
+        public B() { Console.WriteLine("Derived class constructor"); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B obj = new B();
+        }
+    }
+}
