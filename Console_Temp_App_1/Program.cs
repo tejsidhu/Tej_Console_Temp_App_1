c175ff3eb981469a9243e6197629d532d8adbbc1
Tejpartap Singh <singh.tejpartap@gmail.com>
2019-11-22 14:37:25 +1100

    Enter the branch no

--- Console_Temp_App_1/Program.cs
+++ Console_Temp_App_1/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿//test Branch 1
+
+using System;
 
 namespace Console_Temp_App_1
 {
+    //Example 1: Constructors
+    /*
     abstract class A
     {
         protected A() { Console.WriteLine("Abstract class constructor"); }
@@ -17,6 +21,43 @@ namespace Console_Temp_App_1
         static void Main(string[] args)
         {
             B obj = new B();
+        }
+    }
+    */
+
+    //Example 2: Delegate evolution
+    public class test
+    {
+        delegate void TestDelegate(string s);
+        static void M(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static void Main(string[] args)
+        {
+            // Original delegate syntax required 
+            // initialization with a named method.
+            TestDelegate testDelA = new TestDelegate(M);
+
+            // C# 2.0: A delegate can be initialized with
+            // inline code, called an "anonymous method." This
+            // method takes a string as an input parameter.
+            TestDelegate testDelB = delegate (string s) { Console.WriteLine(s); };
+
+            // C# 3.0. A delegate can be initialized with
+            // a lambda expression. The lambda also takes a string
+            // as an input parameter (x). The type of x is inferred by the compiler.
+            TestDelegate testDelC = (x) => { Console.WriteLine(x); };
+
+            // Invoke the delegates.
+            testDelA("Hello. My name is M and I write lines.");
+            testDelB("That's nothing. I'm anonymous and ");
+            testDelC("I'm a famous author.");
+
+            // Keep console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
